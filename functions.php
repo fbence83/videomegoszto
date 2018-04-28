1a52b8beeeb3eade7bd3ec5062b6c46be03d0f93
Faellpeti <gacsi.peti95@gmail.com>
2018-04-28 17:23:58 +0200

    +plust függvények guihoz

--- functions.php
+++ functions.php
@@ -30,6 +30,17 @@ function convertYoutubenagy($string) {
 						);
 					}	
 
+function konvertalnagy($string) {
+if (preg_match('![?&]{1}v=([^&]+)!', $string . '&', $m)) {
+    $video_id = $m[1];
+	
+	$url = 'https://img.youtube.com/vi/'.$video_id.'/maxresdefault.jpg';
+	return $url;
+	}
+
+}			
+					
+					
 function konvertal($string) {
 if (preg_match('![?&]{1}v=([^&]+)!', $string . '&', $m)) {
     $video_id = $m[1];
