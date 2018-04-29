bbad1176734cd287b4824b4d139d66a072c8b3d4
Farkas Bence <h449510@stud.u-szeged.hu>
2018-04-29 21:27:29 +0200

    bootstrap.js a jquery után

--- footer.php
+++ footer.php
@@ -15,9 +15,9 @@
             </div>
         </div>
 
-            <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
             <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
             <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+            <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
             <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
 
