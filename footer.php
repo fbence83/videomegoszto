fa924e976fd26f7453842f39330c9f656dde5069
Faellpeti <gacsi.peti95@gmail.com>
2018-05-12 14:41:35 +0200

    apró változtatások

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
             <div class="footer">
 
                     <ul>
-                    <li><a href= "https://www.facebook.com/">
+                    <li><a href= "https://www.google.com/">
                         <img src="img/ikon1.png" style="width:40px;height:40px;">
                     </a></li>
                     <li><a href= "https://www.facebook.com/">
                         <img src="img/ikon2.png" style="width:40px;height:40px;">
                     </a></li>
-                    <li><a href= "https://www.facebook.com/">
+                    <li><a href= "https://www.twitter.com/">
                         <img src="img/ikon3.png" style="width:40px;height:40px;">
                     </a></li>
                     </ul>
