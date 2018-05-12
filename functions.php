283cd54aca53548993fd0e9f55b9a4634aee91c4
Faellpeti <gacsi.peti95@gmail.com>
2018-05-12 14:42:08 +0200

    fölöst kiszedtem

--- functions.php
+++ functions.php
@@ -7,25 +7,11 @@ if(!isset($_SESSION["admin"])) {
 define("ADMIN", $_SESSION["admin"]);
 include ("dao.php");
 
-function convertYoutube($string) {
-					return preg_replace(
-					"/\s*[a-zA-Z\/\/:\.]*youtu(be.com\/watch\?v=|.be\/)([a-zA-Z0-9\-_]+)([a-zA-Z0-9\/\*\-\_\?\&\;\%\=\.]*)/i",
-						"<iframe width=\"260\" height=\"160\" src=\"//www.youtube.com/embed/$2\" allowfullscreen></iframe>",
-					$string
-						);
-					}	
-					
-function convertYoutube480($string) {
-					return preg_replace(
-					"/\s*[a-zA-Z\/\/:\.]*youtu(be.com\/watch\?v=|.be\/)([a-zA-Z0-9\-_]+)([a-zA-Z0-9\/\*\-\_\?\&\;\%\=\.]*)/i",
-						"<iframe width=\"480\" height=\"360\" src=\"//www.youtube.com/embed/$2\" allowfullscreen></iframe>",
-					$string
-						);
-					}	
+
 function convertYoutubenagy($string) {
 					return preg_replace(
 					"/\s*[a-zA-Z\/\/:\.]*youtu(be.com\/watch\?v=|.be\/)([a-zA-Z0-9\-_]+)([a-zA-Z0-9\/\*\-\_\?\&\;\%\=\.]*)/i",
-						"<iframe width=\"720\" height=\"480\" src=\"//www.youtube.com/embed/$2\" allowfullscreen></iframe>",
+						"<iframe width=\"720\" height=\"480\" src=\"//www.youtube.com/embed/$2\"  id = \"felugrovidi\"></iframe>",
 					$string
 						);
 					}	
